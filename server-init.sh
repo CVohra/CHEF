fbc25f123cdb27b1a9531a66f75b534f6e2a7968
Raghu K <raghuk.vit@gmail.com>
2018-08-07 02:14:11 +0000

    Updating CHEF

--- server-init.sh
+++ server-init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 NAME=$(hostname)
+if [ $(hostname | cut -c 4-6) == "stg" ]; then 
+  ENV=staging
+elif [ $(hostname | cut -c 4-6) == "prd" ]; then 
+  ENV=prod
+else
+  exit 
+fi
 sed -i -e "/node_name/ c node_name \"$NAME\" " /etc/chef/client.rb
+sed -i -e "$ a environment \"$ENV\"" /etc/chef/client.rb
 chef-client 
